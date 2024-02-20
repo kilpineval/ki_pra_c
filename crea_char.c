71e99dc10f62d10f3612e0559384c24a4b17c977
kilpinval <kilpinval@gmail.com>
2024-02-19 21:52:13 -0500

    Create crea_char.c
    septi_commi

---
+++ crea_char.c
@@ -0,0 +1,7 @@
+include <iostream>
+include <libstd.h>
+include <conio.h>
+char file[80] = "ceamos un arcchivo " +w;
+FILE * ptr
+for (i=0; i<100; i++)
+printf("el archivo es de tipo '\%s'\ y se abrio de manera correcta");
