ccc99087f41094870d8a5fc1706a35aec86473bb
kilder pineda <kilpinval@gmail.com>
2024-04-16 22:28:31 -0500

    my tree

--- frecuencimetro.c
+++ frecuencimetro.c
@@ -1,16 +1,17 @@
-include<16f876.h>
-use delay(clock=4000000
-fuses NOWDT,XT,NOPROTECT,NOLVP
+#include <conio.h>
+use delay(clock=4000000);
+fuses ,XT,NOPROTECT,NOLVP;
 
-DEFINE USE_PORT_KBD TRUE)
-VARIABLE rbd_data __FLOAT_WORD_ORDER__
-PROCEDURE int main(int argc, char const *argv[])
-{
+iDEFINE USE_PORT_KBD 
+bd_data __FLOAT_WORD_ORDER__;
+
+int main(int argc, char const *argv[]){
+
     /* code */
     int main(){
         for (i=0;i<0;i++){
             int i=10;
-            in E=7+i*5);
+            int E=7+i*5;
             
         }
     }
